1a358ef9e618ff6e0d901eb94f11fb2176e3c45a
Marken <27118779+Marken2808@users.noreply.github.com>
2020-07-28 13:11:41 +0100

    navigated signin signup and home

--- src/resources/controllers/SignInController.java
+++ src/resources/controllers/SignInController.java
@@ -8,10 +8,15 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import resources.mySQLconnection;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -40,36 +45,40 @@ public class SignInController implements Initializable {
     void makeLogin(ActionEvent event) {
         connection = mySQLconnection.ConnectDataBase();
         String query = "Select * from users where username = ? and password = ?";
-                try{
-                    pst = connection.prepareStatement(query);
-                    pst.setString(1,user.getText());
-                    pst.setString(2,pass.getText());
-                    rs = pst.executeQuery();
-                    if(rs.next()){
-                        JOptionPane.showMessageDialog(null,"Username and Password is correct");
+            try{
+                pst = connection.prepareStatement(query);
+                pst.setString(1,user.getText());
+                pst.setString(2,pass.getText());
+                rs = pst.executeQuery();
+                if(rs.next()){
+                    JOptionPane.showMessageDialog(null,"Username and Password is correct");
 
-                    }else{
-                        JOptionPane.showMessageDialog(null,"Incorrect user");
+                    signIn.getScene().getWindow().hide();
+                    Parent homeRoot = FXMLLoader.load(getClass().getResource("/resources/FilesFXML/HomeScreen.fxml"));
+                    Stage homeStage = new Stage();
+                    Scene homeScene = new Scene(homeRoot);
+                    homeStage.setScene(homeScene);
+                    homeStage.show();
+                }else{
+                    JOptionPane.showMessageDialog(null,"Incorrect user");
 
-                    }
                 }
-                catch ( Exception e){
-                    JOptionPane.showMessageDialog(null,e);
+            }
+            catch ( Exception e){
+                JOptionPane.showMessageDialog(null,e);
 
-                }
+            }
     }
 
-//    void makeLogin(ActionEvent e) {
-//        String username =  user.getText();
-//        String password = pass.getText();
-//        //System.out.println(username + password);
-//        if ( username.equals("zxc")&&password.equals("qwe")){
-//            System.out.println("Correct");
-//        }
-//        else{
-//            System.out.println("Wrong");
-//        }
-//    }
+    @FXML
+    void letRegister(ActionEvent e) throws IOException {
+        signUp.getScene().getWindow().hide();
+        Parent signUpRoot = FXMLLoader.load(getClass().getResource("/resources/FilesFXML/SignUpScreen.fxml"));
+        Stage signUpStage = new Stage();
+        Scene signUpScene = new Scene(signUpRoot);
+        signUpStage.setScene(signUpScene);
+        signUpStage.show();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/resources/controllers/SignUpController.java
+++ src/resources/controllers/SignUpController.java
@@ -3,16 +3,32 @@ package resources.controllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import resources.mySQLconnection;
 
+import javax.swing.*;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ResourceBundle;
 
 public class SignUpController implements Initializable {
+
+    @FXML
+    private JFXTextField new_user;
+
+    @FXML
+    private JFXTextField new_pass;
+
+    @FXML
+    private JFXTextField check_pass;
+
     @FXML
     private JFXDatePicker datePicker;
 
@@ -31,6 +47,27 @@ public class SignUpController implements Initializable {
         double age = p.getYears();
         ageSlider.setValue(age);
 
+    }
+
+    Connection connection = null;
+    ResultSet rs = null;
+    PreparedStatement pst =null;
+
+    @FXML
+    void makeRegister(ActionEvent event) {
+        connection = mySQLconnection.ConnectDataBase();
+        String query = "insert into users (username,password) values (?,?)";
+        try{
+            pst = connection.prepareStatement(query);
+            pst.setString(1,new_user.getText());
+            pst.setString(2,new_pass.getText());
+            pst.execute();
+            JOptionPane.showMessageDialog(null,"Saved");
+        }
+        catch ( Exception e){
+            JOptionPane.showMessageDialog(null,e);
+
+        }
     }
 
     @Override
