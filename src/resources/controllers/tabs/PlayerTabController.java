2b16b612b12ed19d94705ce2142ce1dd1eaca97f
Marken <27118779+Marken2808@users.noreply.github.com>
2020-08-19 17:34:45 +0100

    make seperated table view and controlpanel
    easy to view all detail when click

--- src/resources/controllers/tabs/PlayerTabController.java
+++ src/resources/controllers/tabs/PlayerTabController.java
@@ -11,7 +11,9 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import resources.controllers.functions.Players;
 
 import java.net.URL;
@@ -30,6 +32,33 @@ public class PlayerTabController implements Initializable {
 
     @FXML
     private JFXComboBox<String> posBox;
+
+    @FXML
+    private JFXTextField selectedName;
+
+    @FXML
+    private JFXTextField selectedClub;
+
+    @FXML
+    private JFXTextField selectedPos;
+
+    @FXML
+    private JFXTextField selectedPace;
+
+    @FXML
+    private JFXTextField selectedShooting;
+
+    @FXML
+    private JFXTextField selectedPassing;
+
+    @FXML
+    private JFXTextField selectedAgility;
+
+    @FXML
+    private JFXTextField selectedDefending;
+
+    @FXML
+    private JFXTextField selectedPhysical;
 
     @FXML
     private TextField accelerationBox;
@@ -167,6 +196,9 @@ public class PlayerTabController implements Initializable {
     private JFXSlider slidingTackleSlider;
 
     @FXML
+    private StackPane controlPlayerPane;
+
+    @FXML
     private TreeTableView<Players> treePlayer;
 
     @FXML
@@ -205,7 +237,16 @@ public class PlayerTabController implements Initializable {
     @FXML
     private JFXButton editBtn;
 
+    @FXML
+    private ImageView hideBtn;
 
+    @FXML
+    private JFXButton closeBtn;
+
+    @FXML
+    void hidePane(MouseEvent event) {
+        controlPlayerPane.setVisible(false);
+    }
 
 
     @FXML
@@ -234,15 +275,15 @@ public class PlayerTabController implements Initializable {
                 ") values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         try {
             pst = connection.prepareStatement(query);
-            pst.setString(1,nameField.getText());
-            pst.setString(2,clubField.getText());
-            pst.setString(3,posBox.getValue());
-            pst.setDouble(4,accelerationSlider.getValue());
-            pst.setDouble(5,sprintSpeedSlider.getValue());
-            pst.setDouble(6,positioningSlider.getValue());
-            pst.setDouble(7,finishingSlider.getValue());
-            pst.setDouble(8,shotPowerSlider.getValue());
-            pst.setDouble(9,longShotSlider.getValue());
+            pst.setString(1, nameField.getText());
+            pst.setString(2, clubField.getText());
+            pst.setString(3, posBox.getValue());
+            pst.setDouble(4, accelerationSlider.getValue());
+            pst.setDouble(5, sprintSpeedSlider.getValue());
+            pst.setDouble(6, positioningSlider.getValue());
+            pst.setDouble(7, finishingSlider.getValue());
+            pst.setDouble(8, shotPowerSlider.getValue());
+            pst.setDouble(9, longShotSlider.getValue());
             pst.setDouble(10,volleysSlider.getValue());
             pst.setDouble(11,penaltiesSlider.getValue());
             pst.setDouble(12,visionSlider.getValue());
@@ -274,9 +315,8 @@ public class PlayerTabController implements Initializable {
     @FXML
     void updateDB(MouseEvent event) {
         try {
-            String newName = nameField.getText();
             String sql = "Update players Set " +
-                    "name = '"+newName+"', " +
+                    "name = '"+nameField.getText()+"', " +
                     "club = '"+clubField.getText()+"', " +
                     "position = '"+posBox.getValue()+"', " +
                     "acceleration= '"+accelerationSlider.getValue()+"', " +
@@ -306,7 +346,7 @@ public class PlayerTabController implements Initializable {
                     "jumping= '"+jumpingSlider.getValue()+"'," +
                     "strength= '"+strengthSlider.getValue()+"'," +
                     "aggression= '"+aggressionSlider.getValue()+"' " +
-                    "where name='"+newName+"' ";
+                    "where name='"+nameField.getText()+"' ";
 
             pst= connection.prepareStatement(sql);
             pst.execute();
@@ -314,6 +354,7 @@ public class PlayerTabController implements Initializable {
 
         } catch (Exception e) {
         }
+        setSelectedByRow();
     }
 
     @FXML
@@ -322,6 +363,7 @@ public class PlayerTabController implements Initializable {
         if(event.getClickCount() == 2){
             Players selectedPlayer = treePlayer.getSelectionModel().getSelectedItem().getValue();
             getDataOnRow(selectedPlayer);
+            controlPlayerPane.setVisible(true);
 //            delBtn.setDisable(false);
 //            editBtn.setDisable(false);
 //            addBtn.setDisable(true);
@@ -356,40 +398,92 @@ public class PlayerTabController implements Initializable {
             }
         });
     }
-    
+    void setSelectedByRow(){
+        getUpdateTable();
+        //selectedName.setText(nameField.getText());
+        selectedClub.setText(clubField.getText());
+        selectedPos.setText(posBox.getValue());
+        selectedPace.setText(String.valueOf(
+                (int)(accelerationSlider.getValue()+
+                        sprintSpeedSlider.getValue()
+                )/2
+        ));
+        selectedShooting.setText(String.valueOf(
+                (int)(positioningSlider.getValue()+
+                        finishingSlider.getValue()+
+                        shotPowerSlider.getValue()+
+                        longShotSlider.getValue()+
+                        volleysSlider.getValue()+
+                        penaltiesSlider.getValue()
+                )/6
+        ));
+        selectedPassing.setText(String.valueOf(
+                (int)(visionSlider.getValue()+
+                        crossingSlider.getValue()+
+                        freeKickSlider.getValue()+
+                        shortPassingSlider.getValue()+
+                        longPassingSlider.getValue()+
+                        curveSlider.getValue()
+                )/6
+        ));
+        selectedAgility.setText(String.valueOf(
+                (int)(agilitySlider.getValue()+
+                        balanceSlider.getValue()+
+                        reactionsSlider.getValue()+
+                        ballControlSlider.getValue()+
+                        dribblingSlider.getValue()
+                )/5
+        ));
+        selectedDefending.setText(String.valueOf(
+                (int)(interceptionsSlider.getValue()+
+                        headingSlider.getValue()+
+                        markingSlider.getValue()+
+                        standTackleSlider.getValue()+
+                        slidingTackleSlider.getValue()
+                )/5
+        ));
+        selectedPhysical.setText(String.valueOf(
+                (int)(jumpingSlider.getValue()+
+                      strengthSlider.getValue()+
+                      aggressionSlider.getValue()
+                )/3
+        ));
+    }
+
     void getDataOnRow(Players pl){
         nameField.setText(pl.getPname());
         clubField.setText(pl.getPclub());
         posBox.setValue(pl.getPosition());
 
-        setDataShow(accelerationBox,accelerationSlider,pl.getAcceleration());
-        setDataShow(sprintSpeedBox,sprintSpeedSlider,pl.getSprintspeed());
-        setDataShow(jumpingBox, jumpingSlider,pl.getJumping());
-        setDataShow(strengthBox, strengthSlider,pl.getStrength());
-        setDataShow(aggressionBox, aggressionSlider,pl.getAggression());
-        setDataShow(positioningBox, positioningSlider,pl.getPositioning());
-        setDataShow(finishingBox, finishingSlider,pl.getFinishing());
-        setDataShow(shotPowerBox, shotPowerSlider,pl.getShotpower());
-        setDataShow(longShotBox, longShotSlider,pl.getLongshot());
-        setDataShow(volleysBox, volleysSlider,pl.getVolleys());
-        setDataShow(penaltiesBox, penaltiesSlider,pl.getPenalties());
-        setDataShow(visionBox, visionSlider,pl.getVision());
-        setDataShow(crossingBox, crossingSlider,pl.getCrossing());
-        setDataShow(freeKickBox, freeKickSlider,pl.getFreekick());
-        setDataShow(shortPassingBox, shortPassingSlider,pl.getShortpassing());
-        setDataShow(longPassingBox, longPassingSlider,pl.getLongpassing());
-        setDataShow(curveBox, curveSlider,pl.getCurve());
-        setDataShow(agilityBox, agilitySlider,pl.getAgility());
-        setDataShow(balanceBox, balanceSlider,pl.getBalance());
-        setDataShow(reactionsBox, reactionsSlider,pl.getReactions());
-        setDataShow(ballControlBox, ballControlSlider,pl.getBallcontrol());
-        setDataShow(dribblingBox, dribblingSlider,pl.getDribbling());
-        setDataShow(interceptionsBox, interceptionsSlider,pl.getInterceptions());
-        setDataShow(headingBox, headingSlider,pl.getHeading());
-        setDataShow(markingBox, markingSlider,pl.getMarking());
-        setDataShow(standTackleBox, standTackleSlider,pl.getStandtackle());
-        setDataShow(slidingTackleBox, slidingTackleSlider,pl.getSlidingtackle());
+        setDataShow(accelerationBox     , accelerationSlider    ,pl.getAcceleration());
+        setDataShow(sprintSpeedBox      , sprintSpeedSlider     ,pl.getSprintspeed());
+        setDataShow(jumpingBox          , jumpingSlider         ,pl.getJumping());
+        setDataShow(strengthBox         , strengthSlider        ,pl.getStrength());
+        setDataShow(aggressionBox       , aggressionSlider      ,pl.getAggression());
+        setDataShow(positioningBox      , positioningSlider     ,pl.getPositioning());
+        setDataShow(finishingBox        , finishingSlider       ,pl.getFinishing());
+        setDataShow(shotPowerBox        , shotPowerSlider       ,pl.getShotpower());
+        setDataShow(longShotBox         , longShotSlider        ,pl.getLongshot());
+        setDataShow(volleysBox          , volleysSlider         ,pl.getVolleys());
+        setDataShow(penaltiesBox        , penaltiesSlider       ,pl.getPenalties());
+        setDataShow(visionBox           , visionSlider          ,pl.getVision());
+        setDataShow(crossingBox         , crossingSlider        ,pl.getCrossing());
+        setDataShow(freeKickBox         , freeKickSlider        ,pl.getFreekick());
+        setDataShow(shortPassingBox     , shortPassingSlider    ,pl.getShortpassing());
+        setDataShow(longPassingBox      , longPassingSlider     ,pl.getLongpassing());
+        setDataShow(curveBox            , curveSlider           ,pl.getCurve());
+        setDataShow(agilityBox          , agilitySlider         ,pl.getAgility());
+        setDataShow(balanceBox          , balanceSlider         ,pl.getBalance());
+        setDataShow(reactionsBox        , reactionsSlider       ,pl.getReactions());
+        setDataShow(ballControlBox      , ballControlSlider     ,pl.getBallcontrol());
+        setDataShow(dribblingBox        , dribblingSlider       ,pl.getDribbling());
+        setDataShow(interceptionsBox    , interceptionsSlider   ,pl.getInterceptions());
+        setDataShow(headingBox          , headingSlider         ,pl.getHeading());
+        setDataShow(markingBox          , markingSlider         ,pl.getMarking());
+        setDataShow(standTackleBox      , standTackleSlider     ,pl.getStandtackle());
+        setDataShow(slidingTackleBox    , slidingTackleSlider   ,pl.getSlidingtackle());
 
+        setSelectedByRow();
 
     }
 
@@ -437,7 +531,7 @@ public class PlayerTabController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        controlPlayerPane.setVisible(false);
         getUpdateTable();
 
 
