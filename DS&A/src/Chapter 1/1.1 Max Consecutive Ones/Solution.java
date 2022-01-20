9bce91c86a1ae42830ccb8b841d67ddc7ab72996
Marken <nctuan.ityu@gmail.com>
2022-01-20 20:22:40 +0000

    up

--- DS&A/src/Chapter 1/1.1 Max Consecutive Ones/Solution.java
+++ DS&A/src/Chapter 1/1.1 Max Consecutive Ones/Solution.java
@@ -1,30 +1,42 @@
 class Solution {
 
-//    Runtime: 3 ms
-//    Memory Usage: 53.7 MB
+////    Runtime: 3 ms
+////    Memory Usage: 53.7 MB
+//    public static int findMaxConsecutiveOnes(int[] nums) {
+//
+//        int count = 0;
+//        int max = 0;
+//
+//        for(int i=0; i<nums.length; i++) {
+//            if(nums[i]==1) {
+//                count++;
+//            } else {
+//                count = 0;
+//            }
+//
+//            if (count>max) {
+//                max = count;
+//            }
+//        }
+//        System.out.println(max);
+//        return max;
+//    }
+
+//    Runtime: 2 ms
+//    Memory Usage: 40.7 MB
     public static int findMaxConsecutiveOnes(int[] nums) {
 
-        int count = 0;
-        int max = 0;
+        int maxHere = 0, max = 0;
+        for (int n : nums)
+            max = Math.max(max, maxHere = n == 0 ? 0 : maxHere + 1);
 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i]==1) {
-                count++;
-            } else {
-                count = 0;
-            }
-
-            if (count>max) {
-                max = count;
-            }
-        }
+        System.out.println(max);
         return max;
     }
 
     public static void main (String[] args) {
-        findMaxConsecutiveOnes(new int[]{});
+        findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1});
     }
-
 }
 
 
