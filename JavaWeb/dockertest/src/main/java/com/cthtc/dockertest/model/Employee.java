5168a6820d3e21cc90e1858eaa22d3cddd8ad8ed
Marken <nctuan.ityu@gmail.com>
2022-01-24 00:50:47 +0000

    init jhipster

---
+++ JavaWeb/dockertest/src/main/java/com/cthtc/dockertest/model/Employee.java
@@ -0,0 +1,15 @@
+package com.cthtc.dockertest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+}
