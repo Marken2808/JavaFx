f98ce05259faf8d488330f093fa7e183c230f9a3
Marken <nctuan.ityu@gmail.com>
2022-02-08 01:00:10 +0000

    Initial version of shops generated by generator-jhipster@7.6.0

---
+++ JavaWeb/shops-jhipster/src/main/java/com/cthtc/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cthtc.repository;
+
+import com.cthtc.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ JavaWeb/shops-jhipster/src/main/java/com/cthtc/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.cthtc.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
