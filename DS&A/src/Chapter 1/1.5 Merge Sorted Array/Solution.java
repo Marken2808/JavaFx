3ff55e930b61df08af3beaf4a65248d1de1a93f5
Marken <nctuan.ityu@gmail.com>
2022-03-03 14:27:09 +0000

    done 1.5, too lazy

--- DS&A/src/Chapter 1/1.5 Merge Sorted Array/Solution.java
+++ DS&A/src/Chapter 1/1.5 Merge Sorted Array/Solution.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 class Solution {
 
+//    Runtime: 3 ms
+//    Memory Usage: 43.3 MB
     public static int[] clone(int[] nums, int length) {
         int[] clone = new int[length];
         for (int i=0; i < nums.length; i++) {
@@ -14,20 +16,16 @@ class Solution {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
-
         int[] nums1clone = clone(nums1,m);
         int[] nums2clone = clone(nums2,n);
 
-        nums1 = new int[m+n];
-
-//        System.out.println(Arrays.toString(clone(nums2,n)));
-        System.arraycopy(nums2clone,0,nums1,m,n);
 //        System.out.println(Arrays.toString(clone(nums1,m)));
         System.arraycopy(nums1clone,0,nums1,0,m);
+//        System.out.println(Arrays.toString(clone(nums2,n)));
+        System.arraycopy(nums2clone,0,nums1,m,n);
+
 
         Arrays.sort(nums1);
-//        nums1 = new int[m+n];
-//        System.arraycopy(merge,0,nums1,0,merge.length);
         System.out.println(Arrays.toString(nums1));
     }
 
