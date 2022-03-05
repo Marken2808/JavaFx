709f398005b0780467f10b82419a90966637f649
Marken <nctuan.ityu@gmail.com>
2022-03-05 15:34:17 +0000

    Update Solution.java

--- DS&A/src/Chapter 1/1.9 Valid Mountain Array/Solution.java
+++ DS&A/src/Chapter 1/1.9 Valid Mountain Array/Solution.java
@@ -5,30 +5,42 @@ import java.util.Map;
 
 class Solution {
 
-    public static int findPerkIndex(int[] arr){
-        int maxAtIndex = 0;
-        int maxAtValue = 0;
-        for(int i=0; i< arr.length; i++){
-            if(arr[i] > maxAtValue) {
-                maxAtValue = arr[i];
-                maxAtIndex = i;
-            }
-        }
-        return maxAtIndex;
+////    Runtime: 3 ms
+////    Memory Usage: 54.7 MB
+//    public static int findPerkIndex(int[] arr){
+//        int maxAtIndex = 0;
+//        int maxAtValue = 0;
+//        for(int i=0; i< arr.length; i++){
+//            if(arr[i] > maxAtValue) {
+//                maxAtValue = arr[i];
+//                maxAtIndex = i;
+//            }
+//        }
+//        return maxAtIndex;
+//    }
+//    public static boolean validMountainArray(int[] arr) {
+//        int perk = findPerkIndex(arr);
+//        if(arr.length>2 && perk!=(arr.length-1) && perk!=0){
+//            for(int i=0; i<perk-1; i++){
+//                if(arr[i]>=arr[i+1] ) return false;
+//            }
+//            for (int i=perk; i<arr.length-1; i++){
+//                if(arr[i]<=arr[i+1]) return false;
+//            }
+//            return true;
+//        }
+//        return false;
+//    }
 
-    }
+//    Runtime: 2 ms
+//    Memory Usage: 54.2 MB
     public static boolean validMountainArray(int[] arr) {
-        int perk = findPerkIndex(arr);
-        if(arr.length>2 && perk!=(arr.length-1) && perk!=0){
-            for(int i=0; i<perk-1; i++){
-                if(arr[i]>=arr[i+1] ) return false;
-            }
-            for (int i=perk; i<arr.length-1; i++){
-                if(arr[i]<=arr[i+1]) return false;
-            }
-            return true;
-        }
-        return false;
+        if(arr.length < 3) return false;
+        int l = 0;
+        int r = arr.length - 1;
+        while(l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
+        while(r - 1 > 0 && arr[r] < arr[r - 1]) r--;
+        return l == r;
     }
 
     public static void main(String[] args) {
